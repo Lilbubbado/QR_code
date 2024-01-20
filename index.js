83c957fb39eb6b2bacd7efd1af40f8b467255739
Adam <ajlocke@live.com>
2024-01-20 16:18:36 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+
+inquirer
+  .prompt([
+    {
+      name: "URL",
+      message: "Please enter your link to generate a QR code:",
+    },
+  ])
+  .then((answer) => {
+    console.log(answer.URL);
+    const url = answer.URL;
+
+    var qr_png = qr.image(url);
+    qr_png.pipe(fs.createWriteStream('qr_code.png'));
+
+
+    fs.writeFile("myURL.txt", url, (err) => {
+        if (err) throw err;
+        console.log("The file has been saved!")
+    })
+  })
+    .catch((error) => { //this is an error log (left empty here)
+        if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+        } else {
+        // Something else went wrong
+        }
+    });
+ 
